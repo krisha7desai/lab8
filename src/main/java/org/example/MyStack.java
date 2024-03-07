a2fd70c590c026e86595048cfd8cbd46e4e6ad8a
krisha7desai <krishad2001@gmail.com>
2024-03-07 00:37:37 -0500

    Removing ArrayList

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,26 +3,26 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E> {
+public class MyStack<E> {
     private ArrayList<E> delegate;
     public MyStack() {
-        delegate = this;
+        this.delegate = new ArrayList<>();
     }
 
     public void push(E e) {
 
-        this.add(e);
+        delegate.add(e);
     }
 
     public E pop() {
-        if (this.isEmpty()) throw new EmptyStackException();
-        E e = this.get(size() - 1);
-        this.remove(size() -1 );
+        if (delegate.isEmpty()) throw new EmptyStackException();
+        E e = delegate.get(delegate.size() - 1);
+        delegate.remove(delegate.size() -1 );
         return e;
     }
     public E peek() {
-        if (this.isEmpty()) throw new EmptyStackException();
-        return this.get(size() - 1);
+        if (delegate.isEmpty()) throw new EmptyStackException();
+        return delegate.get(delegate.size() - 1);
     }
 
 
